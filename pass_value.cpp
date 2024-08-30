c757b2685ac13aaec2cf384d3e90a29d5b5312ef
divyansh7705 <divyanshrastogi91@gmail.com>
2024-08-31 02:18:04 +0530

    Add files via upload

---
+++ pass_value.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+void swap (int x,int y)
+{
+    int temp;
+    temp=x;
+    x=y;
+    y=temp;
+}
+int main(){
+    int a=5,b=2;
+    swap(a, b);
+     cout<<"Value of a is :"<<a<<endl;
+    cout<<"Value of b is :"<<b<<endl;
+    return 0;
+}
